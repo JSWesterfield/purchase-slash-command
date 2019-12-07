210622be9b61be49c5875760f1f5e0699e548d4f
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2019-12-06 19:24:19 -0500

    Added ngrok and bodyparser middleware
    * added bodyParser middleware parser for json and urlEncoded data
    * set up ngrok to create tunnel for localhost to display updates from
    changes

--- purchase-slash-command.js
+++ purchase-slash-command.js
@@ -1,6 +1,12 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// allow us to easily process POST requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.post("/purchase", async (req, res) => {
   console.log(req);
 });
