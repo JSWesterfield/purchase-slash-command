798d26b5493eb37006bd65c455ee99f6a366c123
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2019-12-09 13:49:25 -0500

    Adjusts response messages sent
    * adds fun emojis using emojisense library for VSCode
    * elaborates a bit better on what happens when you send a request

--- purchase-slash-command.js
+++ purchase-slash-command.js
@@ -7,11 +7,57 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.post('/guest-tracking', async (req, res) => {
+  //console.log(req);
+  console.log(req.body);
+  res.send('Greetings, you added a new User!🎉👋');
+});
+
 app.post('/purchase', async (req, res) => {
-  console.log(req);
+  //console.log(req);
+  console.log(req.body);
+  res.send('Ok, we created an order for you. 🎉👍');
 });
 
 const PORT = 9647;
 app.listen(PORT, () => {
   console.log(`Slack bot server has started on port ${PORT}`);
 });
+
+//Example will be 
+// [guest email]
+// [channel]
+
+//Total Inputs in Form
+// [guest email]
+// [company/organization]
+// [business justification]
+// [workspace]
+// [channel name(s)]
+// [end date]
+// [urgency]
+// [guest name]
+// [created by]
+// [guest exists already?]
+// [account creation date]
+// [requester]
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// // allow us to easily process POST requests
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.post('/purchase', async (req, res) => {
+//   //console.log(req);
+//   console.log(req.body);
+//   res.send('OK, request sent!');
+// });
+
+// const PORT = 9647;
+// app.listen(PORT, () => {
+//   console.log(`Slack bot server has started on port ${PORT}`);
+// });
