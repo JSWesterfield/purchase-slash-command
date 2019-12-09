d47daf36e575412087ebc04c01adf579f1d5c8c0
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2019-12-09 17:00:56 -0500

    Helps with Aysnc functionality with
    * installed Axios
    * added a try catch
    * in the try we create a response varaible and that is set to async
    wait
    * pass in error object to throw an error if it cant complete the try
    * userId and message in the parameters for the sendDM async function

--- modules/slack.js
+++ modules/slack.js
@@ -1,1 +1,9 @@
-//const sendDM = async 
+const axios = require('axios');
+
+const sendDM = async (userId, message) => {
+try {
+    const response = async wait;
+} catch (error) {
+    throw error;
+}
+}
