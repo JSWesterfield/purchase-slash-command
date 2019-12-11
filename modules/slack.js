a74c0e0df2caa4e312dcf58c4443e81160640586
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2019-12-11 16:38:08 -0500

    add axios object to post request within sendDM and fixed bug within the package.json

--- modules/slack.js
+++ modules/slack.js
@@ -1,16 +1,15 @@
 import { post } from 'axios';
 import * as config from '../config';
 
-const sendDM = async (userId) => {
+const sendDM = async (userId, message) => {
 try {
-    const response = await post(
+    const response = await axios.post(
         'https://slack.com/api/conversations.open',
         {
             users: userId
         },
         {
             headers: {
-                //'Authorization': `Bearer ${config.greetBotToken}`,
                 Authorization: `Bearer ${config.botToken}`
             }
         }
